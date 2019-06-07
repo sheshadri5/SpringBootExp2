524cb327d5aa2ba17b2aae9d81c1074569b114ea
sreelekhasandy <sreelekhasandy@192.168.0.3>
2019-06-07 13:02:45 +0530

    its my second project

---
+++ src/main/java/com/app/SpringInputExp2Application.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInputExp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInputExp2Application.class, args);
+		System.out.println("siva");
+	}
+
+}
